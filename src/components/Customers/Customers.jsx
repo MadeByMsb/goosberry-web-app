4ed6ec85e439ee2a485f617e6144ea840fd57be5
Mishab <85479103+imishab@users.noreply.github.com>
2023-09-23 22:26:30 +0530

    logo added

--- src/components/Customers/Customers.jsx
+++ src/components/Customers/Customers.jsx
@@ -4,43 +4,20 @@ import './Customers.scss';
 export default function Customers() {
   return (
     <section className="customers-section">
-        <h1 className="section--title">Customers</h1>
-        <div className="cards-wrapper">
-    <div className="cards-slider">
-        {
-            [...Array(11)].map((item, index) => (
-                <div className="card" key={index}>
-                    <img src={`/customers/logo ${index + 1}.png`} alt="" className="customer--logo" />
-                </div>
-            ))
-        }
-        {
-            [...Array(6)].map((item, index) => (
-                <div className="card" key={index}>
-                    <img src={`/customers/logo ${index + 12}.png`} alt="" className="customer--logo" />
-                </div>
-            ))
-        }
-    </div>
-    <div className="cards-slider">
-        {
-            [...Array(11)].map((item, index) => (
-                <div className="card" key={index}>
-                    <img src={`/customers/logo ${index + 12}.png`} alt="" className="customer--logo" />
-                </div>
-            ))
-        }
-        {
-            [...Array(6)].map((item, index) => (
-                <div className="card" key={index}>
-                    {/* Add images 26 to 36 to this section */}
-                    <img src={`/customers/logo ${index + 31}.png`} alt="" className="customer--logo" />
-                </div>
-            ))
-        }
-    </div>
-</div>
-
+      <h1 className="section--title">Customers</h1>
+      <div className="cards-wrapper">
+        <div className="cards-slider">
+          {[...Array(39)].map((item, index) => (
+            <div className="card" key={index}>
+              <img
+                src={`/customers/logo ${index + 1}.png`}
+                alt={`Logo ${index + 1}`}
+                className="customer--logo"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
     </section>
-  )
+  );
 }
